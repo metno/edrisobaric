c819368d623981a778cf6b2f4a2a8214674051e3
Lars Falk-Petersen <larsfp@met.no>
2024-01-02 12:38:34 +0100

    Formatting, remove unused import.

--- app/app.py
+++ app/app.py
@@ -29,8 +29,4 @@ app.include_router(routes)
 
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "app:app",
-        host="0.0.0.0",
-        port=5000
-    )
+    uvicorn.run("app:app", host="0.0.0.0", port=5000)
