8a94499aa89d5be77caecdf5e888d25fc2615f08
Lars Falk-Petersen <larsfp@met.no>
2024-01-02 14:47:02 +0100

    Fix correct bbox. Fetch isobaric layers from datafile.

--- app/grib.py
+++ app/grib.py
@@ -4,10 +4,8 @@ from datetime import datetime
 import pytz
 import xarray as xr
 
-# from initialize import get_data_path, get_filename, get_base_url
 
-
-# Define labels in file
+# Define labels in grib file
 TEMPERATURE_LABEL = "t"
 LAT_LABEL = "latitude"
 LON_LABEL = "longitude"
@@ -16,18 +14,29 @@ VWIND_LABEL = "v"
 ISOBARIC_LABEL = "isobaricInhPa"
 
 
-def get_vertical_extent() -> list[str]:
+def get_vertical_extent(dataset: xr.Dataset) -> list[str]:
     """Find and return a list of vertical levels, decending."""
-    # ds[TEMPERATURE_LABEL][ISOBARIC_LABEL].data
-    return ["850", "700", "500", "400", "300", "250", "200", "150", "100", "70"]
+    float_list = dataset[TEMPERATURE_LABEL][ISOBARIC_LABEL].data.tolist()
+    str_list = [str(float_list[i]) for i in range(len(float_list))]
+    return str_list
 
 
-def get_spatial_extent() -> list[float]:
+def get_spatial_extent(dataset: xr.Dataset) -> list[float]:
     """Find and return a list of the spatial extent.
 
-    Order: [max lat, min lat, min long, max long](?).
+    From the standard:
+
+    bbox=minx,miny,maxx,maxy.
+
+    The X and Y coordinates are values in the coordinate system defined by the crs query parameter. If crs is not defined, the values will be assumed to be WGS84 longitude/latitude coordinates and heights will be assumed to be in meters above mean sea level, or below for negative values.
+
     """
-    return [64.25, -1.45, 55.35, 14.51]
+    return [
+        dataset.coords[LON_LABEL].data.min(),
+        dataset.coords[LAT_LABEL].data.min(),
+        dataset.coords[LON_LABEL].data.max(),
+        dataset.coords[LAT_LABEL].data.max(),
+    ]
 
 
 def get_temporal_extent(dataset: xr.Dataset) -> datetime:
@@ -36,5 +45,4 @@ def get_temporal_extent(dataset: xr.Dataset) -> datetime:
         dataset[TEMPERATURE_LABEL].time.data
     )  # 2023-12-13T00:00:00.000000000
     timestamp = datetime.strptime(initial_time, "%Y-%m-%dT%H:00:00.000000000")
-
     return timestamp.replace(tzinfo=pytz.UTC)
